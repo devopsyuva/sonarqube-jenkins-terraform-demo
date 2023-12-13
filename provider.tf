9dd5b4f572bd8e3ae7e6d8cead3e8408f9940970
Sudhams Reddy Duba <dubareddy.383@gmail.com>
2023-12-13 12:38:35 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
+      source = "hashicorp/aws"
+      version = "5.30.0"
     }
   }
 }
+
 provider "aws" {
-  region  = "us-east-1"
-  profile = "default"
+  region = "us-east-1"
+  # Configuration options
 }
