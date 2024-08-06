ab9440286c9ee65c76ab55ec9a1ffe5b1e4d5505
Sudhams Reddy Duba <dubareddy.383@gmail.com>
2024-08-06 08:44:30 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   ami                         = var.image_id     #"ami-09d56f8956ab235b3"
   instance_type               = var.inst_type[1] // or var.inst_type[0], var.inst_type[2]
   count                       = var.inst_count
-  #associate_public_ip_address = true
+  associate_public_ip_address = false
   availability_zone           = var.az_name
   key_name                    = var.keyname
   vpc_security_group_ids      = [aws_security_group.allow_http_ssh.id]
